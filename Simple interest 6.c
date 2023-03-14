23d5d2c98f09256c40f5efcc86e11ccaf5659de3
Shivam <114249028+cyberNOKS@users.noreply.github.com>
2023-03-14 21:05:55 +0530

    Create Simple interest 6.c

---
+++ Simple interest 6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int P,R,T;
+    scanf("%d",&P);
+    scanf("%d",&R);
+    scanf("%d",&T);
+    printf("%d",(P*R*T/100)+P);
+        
+    return 0;
+}
